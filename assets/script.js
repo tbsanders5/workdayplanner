acb757d71450eb2ace2b7464343182a4969d1658
Monk <tbsanders5@gmail.com>
2020-08-03 20:49:50 -0700

    redid columns, simplified code, finished functions

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,52 @@
-let myWorkDay = {
-    "8 AM": '',
-    "9 AM": '',
-    "10 AM": '',
-    "11 AM": '',
-    "12 PM": '',
-    "1 PM": '',
-    "2 PM": '',
-    "3 PM": '',
-    "4 PM": '',
-    "5 PM": '',
-};
+$(document).ready(function () {
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
 
-   $(document).ready(function() {
-    if(!localStorage.getItem('myWorkDay')) {
-        taskUpdate(myWorkDay);
-    } else {
-        taskUpdate(JSON.parse(localStorage.getItem('myWorkDay')));
+
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        
+        localStorage.setItem(time, text);
+    })
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+
+    function hourTracker() {
+        var currentHour = moment().hour();
+
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour)
+
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
     }
+    hourTracker();
 })
-
-$('#currentDay').text(moment().format('dddd') + ", " + moment().format('MMMM Do YYYY, h:mm:ss a'));
+  
+ 
+    
 
